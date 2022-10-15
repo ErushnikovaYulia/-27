7e6aa120f74cb8c0de6b4ea4b85f52ef4d1b3a9d
Yulia Erushnikova <erushnikova.talisman@gmail.com>
2022-10-15 16:46:13 +0500

    task27

---
+++ задача 27.cs
@@ -0,0 +1,22 @@
+﻿// Напишите функцию и запустите ее, которая на вход принимает
+// число и выдает сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSum(int S)
+{
+    int sum = 0;
+
+    while (S > 0)
+    {
+        sum = sum + (S % 10);
+        S = S / 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int N = int.Parse (Console.ReadLine ()!);
+Console.WriteLine ($"Сумма цифр в числе {N} равна {GetSum(N)}. ");
